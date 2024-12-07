01b8c1ecc7ffbec63b04f3980dff85d8f3e8b304
SQUARE-WAVES <michael.behrman@gmail.com>
2024-12-07 11:17:40 -0600

    added some basic channel edits (soling/deleting inserting channels)

--- src/util/channel_mask.rs
+++ src/util/channel_mask.rs
@@ -18,5 +18,36 @@ impl Mask{
   pub fn toggle(&mut self,chan:usize) {
     self.mask ^= 0x01 << chan;
   }
+
+  //this is for when you delete a channel
+  pub fn shift_after(&mut self,chan:usize) {
+    let splitter = (1<<chan) - 1;
+    let post = self.mask & !(splitter);
+    let pre = self.mask & splitter;
+
+    self.mask = pre | (post>>1);
+  }
+
+  //this is for when you solo a channel
+  pub fn solo(&mut self) {
+    self.mask = 1;
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test_shift_after() {
+    let mut z = Mask { mask:0x10101010 };
+
+    z.shift_after(24);
+    assert_eq!(0x08101010,z.mask);
+    z.shift_after(16);
+    assert_eq!(0x04081010,z.mask);
+    z.shift_after(0);
+    assert_eq!(0x02040808,z.mask);
+  }
 }
 
